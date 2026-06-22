7bde0239e7cf091758e3c2e0c97e19f5be8af982
genzo-dev <gabrielenzopdf12@outlook.com>
2026-06-22 10:24:44 -0300

    chore: remove old api layer

--- lib/api.ts
+++
@@ -1,7 +0,0 @@
-import { createServerApi } from "./api/server";
-import { api as clientApi } from "./api/client";
-
-export const api = typeof window === "undefined" ? createServerApi() : clientApi;
-
-export { createServerApi } from "./api/server";
-export { api as apiClient } from "./api/client";
