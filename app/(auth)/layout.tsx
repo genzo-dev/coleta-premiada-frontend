7d4638aecad44b087fe2c49b05aad698dabc400b
genzo-dev <gabrielenzopdf12@outlook.com>
2026-06-23 19:08:23 -0300

    style: improve auth layout pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -16,11 +17,26 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    <div
+      className={clsx(
+        "flex flex-col min-h-screen items-center justify-center",
+        "lg:flex-row lg:items-stretch  lg:justify-around gap-12 lg:gap-0",
+        "bg-gray-300",
+      )}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+      <div className="lg:w-3/5  flex items-center justify-center">
+        <h1 className="text-5xl text-center">Coleta Premiada</h1>
+      </div>
+
+      <div
+        className={clsx(
+          "flex items-center justify-center rounded-xl py-8 px-8",
+          "lg:w-2/5 lg:rounded-none lg:py-0 lg:px-18 xl:px-24",
+          "text-white bg-black",
+        )}
+      >
+        {children}
+      </div>
+    </div>
   );
 }
