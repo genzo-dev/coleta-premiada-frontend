fa77258f61ce074324f0664e5ff8005cf7c8f03f
genzo-dev <gabrielenzopdf12@outlook.com>
2026-06-30 12:35:29 -0300

    feat: add step-card component

---
+++ components/HomePage/StepCard.tsx
@@ -0,0 +1,29 @@
+import { IconType } from "react-icons";
+import { IoDocumentTextOutline } from "react-icons/io5";
+
+type StepCardProps = {
+  titleStep: string;
+  descriptionStep: string;
+  step: string;
+  Icon: IconType;
+};
+
+export default function StepCard({
+  titleStep,
+  descriptionStep,
+  step,
+  Icon,
+}: StepCardProps) {
+  return (
+    <div className="flex flex-col gap-2 border border-gray-200 rounded-lg shadow-lg p-4">
+      <div className="bg-green-200 rounded-lg p-2 inline-flex self-start">
+        <Icon size={24} />
+      </div>
+      <label className="text-sm text-gray-500">Passo {step}</label>
+
+      <h3 className="text-lg font-semibold">{titleStep}</h3>
+      <p>{descriptionStep}</p>
+    </div>
+  );
+}
+// Cadastre-se vinculando seu à inscrição imobiliária do seu imóvel
