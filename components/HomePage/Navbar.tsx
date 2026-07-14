a27469c2d22feaefc65df636d809211bc170f461
genzo-dev <gabrielenzopdf12@outlook.com>
2026-07-13 23:58:05 -0300

    style(ux): improve dashboard link style on navbar

--- components/HomePage/Navbar.tsx
+++ components/HomePage/Navbar.tsx
@@ -1,21 +1,23 @@
-import Link from "next/link";
-import { Button } from "../ui/button";
-import { Leaf, Menu, X } from "lucide-react";
 import { getCurrentUser } from "@/lib/auth/get-current-user";
 import { createSlug } from "@/utils/create-slug";
 import NavbarClient from "./NavbarClient";
 
 export default async function Navbar() {
   const user = await getCurrentUser();
-  const urlProfileUser = createSlug(user?.perfil || "/login");
+  const urlProfileUser = createSlug(user?.perfil || "");
 
   const navItems = [
     { label: "Início", href: "/#start" },
     { label: "Como funciona?", href: "/#howToWork" },
     { label: "Cidades", href: "/#cities" },
     { label: "Comece agora!", href: "/#callToAction" },
-    { label: "Dashboard", href: urlProfileUser },
   ];
 
-  return <NavbarClient user={user} navItems={navItems} urlProfileUser={urlProfileUser} />;
+  return (
+    <NavbarClient
+      user={user}
+      navItems={navItems}
+      urlProfileUser={urlProfileUser}
+    />
+  );
 }
